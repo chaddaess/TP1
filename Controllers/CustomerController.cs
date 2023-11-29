67eac60ae0829efb1971db468b95ef35b37d8e5b
chadda <chadha.essid@insat.ucar.tn>
2023-11-29 17:12:11 +0100

    initial init

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TP1.Models;
+
+namespace TP1.Controllers
+{
+	public class CustomerController : Controller
+	{
+		public List<Customer> customers = new List<Customer>()
+		{
+			new Customer() { Id = 1,Name="chadha"},
+			new Customer() { Id = 2,Name="elyes"},
+
+		};
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Details(int id) {
+
+			Customer c=customers.Find(x => x.Id == id);
+			return View(c);
+
+		}
+	}
+}
